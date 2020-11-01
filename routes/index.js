b0aa7d12f5248ab273afcf1e9fc0cc76b7f59c78
Waqas Akhtar <akhtar.waqas@rocketmail.com>
2020-11-01 16:21:54 +0500

    initialCommit

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'about' });
+});
+
+router.get('/contactus', function(req, res, next) {
+res.render('contactus', { title: 'Contact Us' });
+});
+
+router.get('/projects', function(req, res, next) {
+res.render('projects', { title: 'Projects' });
+});
+
+router.get('/services', function(req, res, next) {
+res.render('services', { title: 'Services' });
+});
+
+
+module.exports = router;
